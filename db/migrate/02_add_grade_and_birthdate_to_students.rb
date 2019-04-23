fd28ca53f9334edbdda2be57431ba5d5a596b889
Audra Tallman <audi.tallman@gmail.com>
2019-04-23 18:54:36 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,10 +1,8 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
   def change
     add_column :artists do |t|
-      t.integer :age
-      t.string :genre
-      t.integer :age
-      t.string :hometown
+      t.integer :grade
+      t.string :birthdate
     end
   end
 end

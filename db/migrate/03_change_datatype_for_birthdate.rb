39e2645f7031f9af6706ceddcd7229fd0a58b0d6
Audra Tallman <audi.tallman@gmail.com>
2019-04-23 19:10:10 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,4 +1,4 @@
-class ChangeDatatype < ActiveRecord::Migration[5.1]
+class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
   def change
     add_column :students, :grade, :integer
     add_column :students, :birthdate, :string
